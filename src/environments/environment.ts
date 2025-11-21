6b551817c3d239b917c70bea7a0b8ad82e53f47f
Jesús Sierra <jesussierra615@gmail.com>
2025-11-20 23:35:32 -0500

    enviroment api_url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    API_URL: 'http://localhost:3000'
+  production: false,
+  API_URL: 'https://2umg9gexmd.execute-api.us-east-2.amazonaws.com',
 };
